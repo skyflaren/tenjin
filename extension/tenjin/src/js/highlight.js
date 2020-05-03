ce717374d00ffee4998e3c0ca7f64feb482fa79a
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-05-03 01:02:26 -0400

    highlighter v3

--- extension/tenjin/src/js/highlight.js
+++ extension/tenjin/src/js/highlight.js
@@ -20,24 +20,33 @@ $(document).ready(_ => {
 
 
 function checkText(elm) {
-	// if (elm==null) return;
-	// console.log(elm,elm[0].nodeType);
-	// if (elm[0].nodeType == Node.TEXT_NODE || (elm.children().length==0 && elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
-	if (elm[0].nodeType == Node.TEXT_NODE || (elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
-		// console.log('here');
-		for (let word of keywords) {
-			console.log((elm.text().match(new RegExp(`\\b${word}\\b`,"gi")) || []).length);
-			elm.html(elm.html().replace(new RegExp(`\\b${word}\\b`,"gi"),`<span class="tenj-word">${word}<div class="tenj-underline"></div></span>`));
-		}
-	} 
-	else 
-	if (elm[0].nodeType == Node.ELEMENT_NODE) {
-		// console.log('there',elm.children());
-		for (let chd of elm.children()) {
-			// console.log(chd==null,$(chd)[0],$(chd)[0].nodeType);
-			checkText($(chd));
-		}
-	} 
-	// else {console.log($(elm)[0].nodeType) }
+	try {
+		// console.log(elm,elm[0].nodeType);
+		// if (elm[0].nodeType == Node.TEXT_NODE || (elm.children().length==0 && elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
+		// if (elm[0].nodeType == Node.TEXT_NODE || elm[0].firstChild.nodeType == Node.TEXT_NODE) {
+		if (elm[0].nodeType == Node.TEXT_NODE || hasTextNode(elm)) {
+			// console.log('here');
+			for (let word of keywords) {
+				console.log((elm.text().match(new RegExp(`\\b${word}\\b`,"gi")) || []).length);
+				elm.html(elm.html().replace(new RegExp(`\\b${word}\\b`,"gi"),`<span class="tenj-word">${word}<div class="tenj-underline"></div></span>`));
+			}
+		} 
+		else 
+		if (elm[0].nodeType == Node.ELEMENT_NODE) {
+			// console.log('there',elm.children());
+			for (let chd of elm[0].childNodes) {
+				// console.log(chd==null,$(chd)[0],$(chd)[0].nodeType);
+				checkText($(chd));
+			}
+		} 
+		// else {console.log($(elm)[0].nodeType) }
+	} catch (e) {}
 }
 
+
+function hasTextNode(elm) {
+	for (let chd of elm[0].childNodes) {
+		if (chd.nodeType == Node.TEXT_NODE) return true;
+	}
+	return false;
+}
