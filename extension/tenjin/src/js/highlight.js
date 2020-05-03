f77b1ce973bffde8067ef48118dc823da96656e3
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-05-02 22:14:56 -0400

    highlighter v2

--- extension/tenjin/src/js/highlight.js
+++ extension/tenjin/src/js/highlight.js
@@ -1,4 +1,4 @@
-let keywords = ["the"];
+let keywords = ["the", "is"];
 // $(".tenj-word-under").append($("<div></div>").addClass(".tenj-underline"));
 
 // $("span").mouseover(_ => {
@@ -14,20 +14,23 @@ let keywords = ["the"];
 
 $(document).ready(_ => {
 	for (let i of $('p')) checkText($(i));
-	console.log($('p').length);
-	checkText($('body'));
+	// console.log($('p').length);
+	// checkText($('body'));
 })
 
 
 function checkText(elm) {
 	// if (elm==null) return;
 	// console.log(elm,elm[0].nodeType);
-	if (elm[0].nodeType == Node.TEXT_NODE || (elm.children().length==0 && elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
+	// if (elm[0].nodeType == Node.TEXT_NODE || (elm.children().length==0 && elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
+	if (elm[0].nodeType == Node.TEXT_NODE || (elm[0].firstChild && elm[0].firstChild.nodeType == Node.TEXT_NODE)) {
 		// console.log('here');
 		for (let word of keywords) {
-			elm.html(elm.text().replace(new RegExp(`\\b${word}\\b`,"gi"),`<span class="tenj-word">${word}<div class="tenj-underline"></div></span>`));
+			console.log((elm.text().match(new RegExp(`\\b${word}\\b`,"gi")) || []).length);
+			elm.html(elm.html().replace(new RegExp(`\\b${word}\\b`,"gi"),`<span class="tenj-word">${word}<div class="tenj-underline"></div></span>`));
 		}
-	} ///else 
+	} 
+	else 
 	if (elm[0].nodeType == Node.ELEMENT_NODE) {
 		// console.log('there',elm.children());
 		for (let chd of elm.children()) {
