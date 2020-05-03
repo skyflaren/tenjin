c0acf6bdf25105f2d0644bbd627845336424808b
Aava Sapkota <aavachhoir@gmail.com>
2020-05-03 07:19:45 -0400

    Update

--- database/Database.java
+++ database/Database.java
@@ -1,11 +1,17 @@
 package database;
  import java.io.*;
-
- 
 public class Database {
     public static void main(String args[]) throws IOException{
-        BufferedReader input = new BufferedReader(new FileReader("en-DB.txt")); 
-        PrintWriter wr = new PrintWriter(new FileWriter("database.txt"));
+        BufferedReader input = new BufferedReader(new FileReader("holder.txt")); 
+        PrintWriter wr = new PrintWriter(new FileWriter("cleanSheet.txt"));
+
+        String [] words = input.readLine().split(","); 
+        for(int i=0; i<words.length; i++){
+            String [] h=words [i].split(":");
+            wr.println(h[0].substring(1,h[0].length()-1)+": obj."+h[0].substring(1,h[0].length()-1)+",");
+        }
+
+        wr.close();
 
         
     }
